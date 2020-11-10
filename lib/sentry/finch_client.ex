f5e1b2d9f6a1039fee961ed2f8ceb983b8035ff3
Wojtek Mach <wojtekmach@users.noreply.github.com>
2020-11-10 12:09:52 +0100

    Remove Sentry.FinchClient.child_spec/0

--- lib/sentry/finch_client.ex
+++ lib/sentry/finch_client.ex
@@ -5,19 +5,6 @@ defmodule Sentry.FinchClient do
 
   @behaviour Sentry.HTTPClient
 
-  def child_spec do
-    opts = [name: Sentry.Finch]
-
-    Supervisor.child_spec(
-      %{
-        id: __MODULE__,
-        start: {Finch, :start_link, [opts]},
-        type: :supervisor
-      },
-      []
-    )
-  end
-
   def post(url, headers, body) do
     request = Finch.build(:post, url, headers, body)
 
